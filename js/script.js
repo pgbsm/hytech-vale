d3b98b17dcc259e250a88443da9fe116b53d5512
pgabriel30 <paulogabriel.bsm@gmail.com>
2023-01-27 16:58:25 -0300

    trabalhando na responsividade

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ btnDrop.onclick = function () {
     btnDrop.value = 'VER MENOS'
     btnDrop.classList.remove(".oculto")
 }
+
+function myFunction() {
+    document.getElementById('myTopnav').classList.toggle('responsive')
+}
+
+function toogle() {
+    document.getElementById('burger').classList.toggle('active')
+}
